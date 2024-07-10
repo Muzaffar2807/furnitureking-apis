61fc7f1e20bd8d64630596cf1ae88576a659e1f0
abdul hadi <>
2024-07-10 20:41:32 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors({
-  origin: "http://localhost:3001",
+  origin: "https://furnitureking-test.netlify.app",
   credentials: true
 }));
 app.use("/", indexRouter);
