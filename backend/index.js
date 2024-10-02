bfff57b2202c7c2b211d7207bac3ebe269689af2
Muzaffar2807 <53538056+Muzaffar2807@users.noreply.github.com>
2024-10-03 04:42:01 +0530

    update fix

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
   cors({
-    origin: "https://hindwala.netlify.app/",
+    origin: "https://hindwala.netlify.app",
     credentials: true,
   })
 ); 
